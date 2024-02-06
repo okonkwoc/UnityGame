d04a62967451b12574a0c096714a0d680b60afc9
okonkwoc <158371218+okonkwoc@users.noreply.github.com>
2024-02-06 18:02:31 -0500

    Add files via upload

--- Vectors.cs
+++ Vectors.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Vectors : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void UseVector2()
     {
-        
+        Vector2 position = new Vector2(3.0f, 2.0f);
+    }
+
+    void UseVector3()
+    {
+        Vector3 position3D = new Vector3(3.0f, 2.0f, 3.0f);
+    }
+
+    void UseStaticProperties()
+    {
+        Vector3 moveForward = Vector3.forward;
+        Vector3 moveUp = Vector3.up;
+        Vector3 moveDown = Vector3.down;
+        Vector3 moveLeft = Vector3.left;
+        Vector3 moveRight = Vector3.right;
     }
 
     // Update is called once per frame
