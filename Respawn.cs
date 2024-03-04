1d2f121ebca6ebdfea8b2709f34c2a869599d825
okonkwoc <158371218+okonkwoc@users.noreply.github.com>
2024-03-04 18:19:11 -0500

    Add files via upload

---
+++ Respawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform respawnPoint;
+
+    public GameObject playerPrefab;
+
+    public void RespwanPlayer()
+    {
+        if {playerPrefab && respawnPoint
+        {
+            Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+        }
+    }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
