2151490717f4cae3a32d38fc0fdc2234285a569b
okonkwoc <158371218+okonkwoc@users.noreply.github.com>
2024-03-27 18:01:07 -0400

    Add files via upload

---
+++ Camera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothspeed = 0.125f;
+
+    public Vector3 offset
+   // // Start is called before the first frame update
+  //  void Start()
+  //  {
+        
+  //  }
+
+   // // Update is called once per frame
+  //  void Update()
+  //  {
+        
+  //  }
+}
